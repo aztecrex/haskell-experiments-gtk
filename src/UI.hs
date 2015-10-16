f24e0bfd3c35d174d00173b7a341cdd761c71a73
Greg Wiley <gwiley@cj.com>
2015-10-16 16:11:25 -0700

    a button in a window

--- src/UI.hs
+++ src/UI.hs
@@ -2,5 +2,19 @@ module UI
     ( go
     ) where
 
+import Graphics.UI.Gtk
+
+hello :: (ButtonClass o) => o -> IO ()
+hello b = set b [buttonLabel := "Hello World"]
+
 go :: IO ()
-go = putStrLn "Go."
+go = do
+  initGUI
+  window <- windowNew
+  button <- buttonNew
+  set window [windowDefaultWidth := 200, windowDefaultHeight := 200,
+              containerChild := button, containerBorderWidth := 10]
+  onClicked button (hello button)
+  onDestroy window mainQuit
+  widgetShowAll window
+  mainGUI
